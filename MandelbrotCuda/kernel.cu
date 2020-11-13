4431ebb422575cc662d51563b8a9a9df46d863b2
AlexRuzin <stan.ruzin@gmail.com>
2020-11-12 20:13:07 -0500

    Adding kernel.cu

---
+++ MandelbrotCuda/kernel.cu
@@ -0,0 +1,193 @@
+#pragma once
+
+#include "frame.h"
+
+#include "cuda_occupancy.h"
+#include "cuda_runtime.h"
+#include "cuda_profiler_api.h"
+
+#define IMAGE_SCALEA 1.0
+#define IMAGE_SCALEB 4.0
+
+#define USE_CUDA_PERFORMANCE_METRICS
+
+__constant__ frame::rgbPixel pixel_colour[16] =
+{
+	{ 66,  30,  15 },{ 25,   7,  26 },{ 9,   1,  47 },{ 4,   4,  73 },
+	{ 0,   7, 100 },{ 12,  44, 138 },{ 24,  82, 177 },{ 57, 125, 209 },
+	{ 134, 181, 229 },{ 211, 236, 248 },{ 241, 233, 191 },{ 248, 201,  95 },
+	{ 255, 170,   0 },{ 204, 128,   0 },{ 153,  87,   0 },{ 106,  52,   3 }
+};
+
+__global__ void mandelbrot_kernel(frame::rgbPixel* image,
+	uint32_t width, uint32_t height,
+	double scale,
+	double cx, double cy)
+{
+	const int i = threadIdx.y + blockIdx.y * blockDim.y;
+	const int j = threadIdx.x + blockIdx.x * blockDim.x;
+
+	if (i >= height || j >= width)
+	{
+		return;
+	}
+
+	const std::uint8_t max_iter = 255;
+	const double y = (i - (height >> 1)) * scale + cy;
+	const double x = (j - (width >> 1)) * scale + cx;
+
+	double zx = hypot(x - 0.25, y);
+
+	if (x < zx - 2.0 * zx * zx + 0.25 || (x + 1.0) * (x + 1.0) + y * y < 0.0625)
+	{
+		return;
+	}
+
+	std::uint8_t iter = 0;
+	double zy, zx2, zy2;
+	zx = zy = zx2 = zy2 = 0.0;
+
+	do {
+		zy = 2.0 * zx * zy + y;
+		zx = zx2 - zy2 + x;
+		zx2 = zx * zx;
+		zy2 = zy * zy;
+	} while (iter++ < max_iter && zx2 + zy2 < 4.0);
+
+	if (iter > 0 && iter < max_iter)
+	{
+		const std::uint8_t colour_idx = iter % 16;
+
+		image[i * width + j] = pixel_colour[colour_idx];
+	}
+}
+
+float elapsedTime = 0;
+
+namespace cuda
+{
+	class cudaKernel {
+	private:
+		double cx, cy;
+		const uint32_t image_size;
+		const uint32_t image_width, image_height;
+		const double scale;
+		frame::image *image;
+
+		frame::rgbPixel *pixelData;
+
+		template<class T, typename... A>
+		error_t launch_kernel(T& kernel, dim3 work, A&&... args)
+		{
+			int device;
+			cudaDeviceProp props;
+			cudaGetDevice(&device);
+			cudaGetDeviceProperties(&props, device);
+
+			int threadBlocks;
+			if (props.major == 2)
+			{
+				threadBlocks = 8;
+			}
+			else if (props.major == 3)
+			{
+				threadBlocks = 16;
+			}
+			else
+			{
+				threadBlocks = 32;
+			}
+
+			int blockSize;
+			std::uint32_t minGridSize;
+			cudaOccupancyMaxPotentialBlockSize((int*)&minGridSize, &blockSize, kernel, 0, 0);
+
+			int maxActiveBlocks = 0;
+			do
+			{
+				cudaOccupancyMaxActiveBlocksPerMultiprocessor(&maxActiveBlocks, kernel, blockSize, 0);
+
+				if (blockSize < props.warpSize || maxActiveBlocks >= threadBlocks)
+				{
+					break;
+				}
+
+				blockSize -= props.warpSize;
+			} while (true);
+
+			int blockSizeDimX, blockSizeDimY;
+			blockSizeDimX = blockSizeDimY = (int)pow(2, ceil(log(sqrt(blockSize)) / log(2)));
+
+			while (blockSizeDimX * blockSizeDimY > blockSize)
+			{
+				blockSizeDimY--;
+			}
+
+			dim3 block(blockSizeDimX, blockSizeDimY);
+			dim3 grid((work.x + block.x - 1) / block.x, (work.y + block.y - 1) / block.y);
+			grid.x = grid.x > minGridSize ? grid.x : minGridSize;
+			grid.y = grid.y > minGridSize ? grid.y : minGridSize;
+
+			//#undef _DEBUG
+#ifdef _DEBUG
+			float occupancy = (maxActiveBlocks * blockSize / props.warpSize) / (float)(props.maxThreadsPerMultiProcessor / props.warpSize);
+
+			std::cout << "Grid of size " << grid.x * grid.y << std::endl;
+			std::cout << "Launched blocks of size " << blockSize << std::endl;
+			std::cout << "Theoretical occupancy " << occupancy * 100.0f << "%" << std::endl;
+#endif
+
+			cudaEvent_t start;
+			cudaEventCreate(&start);
+
+			cudaEvent_t stop;
+			cudaEventCreate(&stop);
+
+			cudaEventRecord(start, 0);
+
+			kernel << < grid, block >> > (std::forward<A>(args)...);
+
+			cudaGetLastError();
+			cudaEventRecord(stop, 0);
+			cudaEventSynchronize(stop);
+
+			cudaEventElapsedTime(&elapsedTime, start, stop);
+
+			cudaEventDestroy(start);
+			cudaEventDestroy(stop);
+
+			cudaProfilerStop();
+
+			return 0;
+		}
+
+	public:
+		cudaKernel(frame::image *image, double cx, double cy) :
+			cx(cx), cy(cy),
+			image_size(image->get_height() * image->get_width() * sizeof(frame::rgbPixel)),
+			scale(IMAGE_SCALEA / (image->get_width() / IMAGE_SCALEB)),
+			image_width(image->get_width()), image_height(image->get_height()),
+			image(image)
+		{
+
+		}
+
+		error_t generate_mandelbrot(void)
+		{
+			cudaMalloc((void**)&this->pixelData, image_size);
+			cudaMemset(pixelData, 0, image_size);
+
+			error_t err = launch_kernel(mandelbrot_kernel,
+				dim3(image_width, image_height), pixelData,
+				image_width, image_height, scale, cx, cy);
+			if (err != 0) {
+				return err;
+			}
+
+			cudaMemcpy(image->get_data(), pixelData, image_size, cudaMemcpyDeviceToHost);
+			cudaFree(pixelData);
+
+			return 0;
+		}
+	};
+}
