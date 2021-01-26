8b0aa8ea343c8aca98bc5d839c9e821625559253
AlexRuzin <stan.ruzin@gmail.com>
2021-01-25 23:52:33 -0500

    Adding core files

---
+++ VisualGraphImager/FractalCore/FractalCore/main.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//#define _WIN32
+#if defined(_WIN32)
+#include "windows.h"
+#endif //__WIN32
+
+#include <stdint.h>
+
+uint32_t main(uint32_t argc, char *argv[])
+{
+
+	return 0;
+}
